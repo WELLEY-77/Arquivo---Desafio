65aeae848a9669e2242f946d6d1e3e4472e99a90
Welley <monteirowelley7@gmail.com>
2023-02-04 14:24:42 -0300

    Iniciando o project

---
+++ desafio_salario.py
@@ -0,0 +1,9 @@
+# 5. você deve solicitar o nome e o salario, armazenar em um arquivo no formato de DIC, apicar 6% de desconto se o salario for maior que R$1.500,00 e mostrar o DIC na tela ao final de tudo.
+
+while True:
+    print('''ESCOLHA UMA OPÇÃO:
+    [ 1 ] - CADASTRAR
+    [ 0 ] - SAIR
+    ''')
+    opcao = int(input('Qual a opção desejada? '))
+    break
